ed1357609da2dedffaa009d6055547840bc1b775
juanjosejjjj <juanjosejjjj@gmail.com>
2024-02-25 20:37:25 -0300

    add typescript to the proyect, update nextjs and react and react dom

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>hiworldS</div>
+    </div>
+  )
+}
